8fde890675c81179a646081235dd5d7a3cf8a60a
hkserge <kserge.helegbe@gmail.com>
2021-01-24 15:21:06 +0100

    struct removed

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,13 +6,13 @@
 /*   By: khelegbe <khelegbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 16:59:38 by khelegbe          #+#    #+#             */
-/*   Updated: 2021/01/19 00:04:03 by khelegbe         ###   ########.fr       */
+/*   Updated: 2021/01/24 15:18:11 by khelegbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-static void		read_file(int fd, char **rem, int *bytes_readed, char **buff)
+void	read_file(int fd, char **remaining, int *bytes_readed, char **buff)
 {
 	char			*copy;
 
@@ -22,47 +22,33 @@ static void		read_file(int fd, char **rem, int *bytes_readed, char **buff)
 		return ;
 	}
 	(*buff)[*bytes_readed] = '\0';
-	copy = *rem;
-	*rem = ft_strjoin(*rem, *buff);
+	copy = *remaining;
+	*remaining = ft_strjoin(*remaining, *buff);
 	free(copy);
 }
 
-static void		ft_alloc_rem(t_rem **rem, char **buff)
-{
-	if (!*rem)
-	{
-		*rem = malloc(sizeof(t_rem));
-		(*rem)->str = malloc(sizeof(char));
-		(*rem)->str[0] = '\0';
-	}
-	*buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-}
-
-int				get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
 	char			*buff;
-	static t_rem	*rem[1024];
+	static char		*remaining[1024];
 	int				bytes_readed;
 
 	bytes_readed = 1;
 	if (fd < 0 || BUFFER_SIZE <= 0 || !line)
 		return (-1);
-	ft_alloc_rem(&rem[fd], &buff);
-	if (!buff || !rem[fd])
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
 		return (-1);
-	while (!ft_is_new_line(rem[fd]->str) && bytes_readed != 0)
+	while (!ft_is_new_line(remaining[fd]) && bytes_readed != 0)
 	{
-		read_file(fd, &rem[fd]->str, &bytes_readed, &buff);
+		read_file(fd, &remaining[fd], &bytes_readed, &buff);
 		if (bytes_readed == -1)
 			return (-1);
 	}
 	free(buff);
-	*line = ft_get_new_line(rem[fd]->str);
-	rem[fd]->str = ft_get_the_rest(rem[fd]->str);
+	*line = ft_get_new_line(remaining[fd]);
+	remaining[fd] = ft_get_the_rest(remaining[fd]);
 	if (bytes_readed == 0)
-	{
-		free(&rem[fd]->str);
 		return (0);
-	}
 	return (1);
 }
